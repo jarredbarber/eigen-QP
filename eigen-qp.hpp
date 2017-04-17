eb5d83869bb4a2c15441f7fa6f830c725ca965f0
Jarred Barber <jpb5082@gmail.com>
2017-04-17 16:54:44 -0400

    Updated license

--- eigen-qp.hpp
+++ eigen-qp.hpp
@@ -1,6 +1,30 @@
 /*
- * Fast quadradic programming template library based on Eigen.
- */
+    EigenQP: Fast quadradic programming template library based on Eigen.
+ 
+    From https://github.com/jarredbarber/eigen-QP
+ 
+    MIT License
+
+    Copyright (c) 2017 Jarred Barber
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 #ifndef _EIGEN_QP_H_
 #define _EIGEN_QP_H_
 
